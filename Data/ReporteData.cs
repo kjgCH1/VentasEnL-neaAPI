ba900d1eb77b03d57e003ff98e6f76feae750587
kjgCH1 <cheskyto22@gmail.com>
2023-10-03 18:24:43 -0600

    Reporte

--- Data/ReporteData.cs
+++ Data/ReporteData.cs
@@ -1,10 +1,46 @@
-﻿using System;
+﻿using Entidades;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Data.SqlClient;
 
-namespace VentasEnLíneaData
+namespace Data
 {
     public  class ReporteData
     {
+        private string connectionString;
+
+        ReporteData(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<Reporte> buscarPedidoCliente(String fecha1, String fecha2)
+        {
+            var connection = new SqlConnection();
+            List<Reporte> reportes = new List<Reporte>();
+            string sql = $"exec sp_buscar_reporte @fecha1='{fecha1}', " +
+                $"@fecha2='{fecha2}'";
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                command.CommandType = System.Data.CommandType.Text;
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Reporte reporte = new Reporte();
+                    reporte.Id = (int)reader["id"];
+                    reporte.Fecha = reader["fecha"].ToString();
+                    reporte.Detalle = reader["detalle"].ToString();
+                    reporte.Administrador = (int)reader["administrador"];
+
+                    reportes.Add(reporte);
+
+                }
+                connection.Close();
+            }
+           
+            return reportes;
+        }
     }
 }
--- Entidades/Reporte.cs
+++ Entidades/Reporte.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace Entidades
 {
-    internal class Reporte
+    public class Reporte
     {
+        public int id;
+        public string detalle;
+        public int administrador;
+        public string fecha;
+
+        public Reporte() { }
+
+        public Reporte(int id, string detalle, int administrador, string fecha)
+        {
+            this.id = id;
+            this.detalle = detalle;
+            this.administrador = administrador;
+            this.fecha = fecha;
+        }
+
+        public int Id { get =>  id; set => id = value; }
+        public string Detalle { get => detalle; set => detalle = value; }
+
+        public int Administrador { get => administrador; set => administrador = value; }
+
+        public string Fecha { get => fecha; set => fecha = value; }
     }
 }
