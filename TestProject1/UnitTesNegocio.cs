1ce0805717ceb459be9b534b37271bf800af1b5d
kjgCH1 <cheskyto22@gmail.com>
2023-10-05 00:31:41 -0600

    kevin

--- TestProject1/UnitTesNegocio.cs
+++ TestProject1/UnitTesNegocio.cs
@@ -1,22 +1,22 @@
 using System;
 using Xunit;
-using Data;
 using Entidades;
+using ReglasDeNegocio;
 
 namespace TestProject1
 {
-    public class UnitTest1
+    public class UnitTesNegocio
     {
         [Fact]
         public void Test1()
         {
             Comunidad comunidad = new Comunidad();
-            comunidad.Nombre = "Coyol";
-            comunidad.Precio = 1000.00;
+            comunidad.Nombre = "Noche Buena";
+            comunidad.Precio = 1500.00;
             string connectionString = "workstation id=WokAndRoll.mssql.somee.com;packet size=4096;user id=chesky22_SQLLogin_1;pwd=44a9mwmwsr;data source=WokAndRoll.mssql.somee.com;persist security info=False;initial catalog=WokAndRoll";
 
-            ComunidadData comunidadData = new ComunidadData(connectionString);
-            comunidadData.crearComunidad(comunidad);
+            ComunidadReglasDeNegocio negocio = new ComunidadReglasDeNegocio(connectionString);
+            negocio.crearComunidad(comunidad);
             
         }
     }
---
+++ VentasEnLíneaAPI/Controllers/ComunidadController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VentasEnLíneaAPI.Controllers
+{
+    public class ComunidadController : Controller
+    {
+        public IActionResult crearComunidadcrearComunidad()
+        {
+
+            return View();
+        }
+    }
+}
