f1d4ed27c825afac12d13fe4cbca3aac5536ae52
kjgCH1 <cheskyto22@gmail.com>
2023-10-05 00:04:53 -0600

    kevin

--- ReglasDeNegocio/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ReglasDeNegocio
-{
-    public class Class1
-    {
-    }
-}
---
+++ ReglasDeNegocio/ComunidadReglasDeNegocio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Data;
+using Entidades;
+
+namespace ReglasDeNegocio
+{
+    public class ComunidadReglasDeNegocio
+    {
+        private string connectionString;
+
+        public ComunidadReglasDeNegocio(string connectionString) { 
+            this.connectionString = connectionString;
+        }
+        public bool crearComunidad(Comunidad comunidad) {
+            ComunidadData data= new ComunidadData(this.connectionString);
+            return data.crearComunidad(comunidad);
+        }
+    }
+}
