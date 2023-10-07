f3371ff6fd6b6dd2433ef91fba357227e755536e
kjgCH1 <cheskyto22@gmail.com>
2023-10-07 11:46:35 -0600

    Update ComunidadController.cs
    Avanves

--- VentasEnLíneaAPI/Controllers/ComunidadController.cs
+++ VentasEnLíneaAPI/Controllers/ComunidadController.cs
@@ -10,12 +10,28 @@ namespace VentasEnLíneaAPI.Controllers
     [Route("[controller]")]//Ensablado de clase
     public class ComunidadController : ControllerBase
     {
-        
         [HttpGet]
-        public string holamundo() {
+        public string BuscarComunidad()
+        {
             string connectionString = "workstation id=WokAndRoll.mssql.somee.com;packet size=4096;user id=chesky22_SQLLogin_1;pwd=44a9mwmwsr;data source=WokAndRoll.mssql.somee.com;persist security info=False;initial catalog=WokAndRoll";
             ComunidadReglasDeNegocio reglasDeNegocio = new ComunidadReglasDeNegocio(connectionString);
-            return JsonConvert.SerializeObject(reglasDeNegocio.buscarComunidad("co"));
+            return JsonConvert.SerializeObject(reglasDeNegocio.buscarComunidad(""));
         }
+
+        [HttpGet("buscarComunidad")]
+        public string BuscarComunidad(string parametroBusqueda)
+        {
+            if (string.IsNullOrEmpty(parametroBusqueda))
+            {
+                return "El parámetro de búsqueda no puede estar vacío.";
+            }
+
+            string connectionString = "workstation id=WokAndRoll.mssql.somee.com;packet size=4096;user id=chesky22_SQLLogin_1;pwd=44a9mwmwsr;data source=WokAndRoll.mssql.somee.com;persist security info=False;initial catalog=WokAndRoll";
+            ComunidadReglasDeNegocio reglasDeNegocio = new ComunidadReglasDeNegocio(connectionString);
+            return JsonConvert.SerializeObject(reglasDeNegocio.buscarComunidad(parametroBusqueda));
+        }
+
+
+
     }
 }
